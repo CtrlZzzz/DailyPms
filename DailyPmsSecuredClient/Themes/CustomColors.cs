6a39c83f75a48e3b7651cf0f88e43400054cea84
Amaury Dewaersegger <amaury@mi8.be>
2021-07-26 16:26:11 +0200

    Add drawer menu

---
+++ DailyPmsSecuredClient/Themes/CustomColors.cs
@@ -0,0 +1,17 @@
+﻿namespace DailyPmsSecuredClient.Themes
+{
+    public class CustomColors
+    {
+        //  General
+        public const string PRIMARY = "#737B9D";
+        public const string SECONDARY = "#AFB7C9";
+        public const string TERTIARY = "#F6F8FA";
+
+        // Transparent
+        public const string TRANSPARENT_WHITE = "rgba(255, 255, 255, 0.5)";
+
+        //  Gradient colors
+        public const string HOME_COLOR_1 = "#FBD27A";
+        public const string HOME_COLOR_2 = "#FCF594";
+    }
+}
