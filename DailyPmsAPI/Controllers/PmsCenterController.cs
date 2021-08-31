99cceecb878e00a3e29e70f1865ac01020955ce5
Amaury Dewaersegger <amaury@mi8.be>
2021-08-31 17:23:17 +0200

    Update pms controller

--- DailyPmsAPI/Controllers/PmsCenterController.cs
+++ DailyPmsAPI/Controllers/PmsCenterController.cs
@@ -30,6 +30,42 @@ namespace DailyPmsAPI.Controllers
             return Ok(centers);
         }
 
+        /// <summary>
+        /// Get a center by its ID
+        /// </summary>
+        /// <param name="id">The ID from the center to get</param>
+        /// <returns></returns>
+        /// <response code="200">The center with the specified ID is returned</response>
+        /// <response code="404">The center with the specified ID does not exist in the Database</response>
+        [HttpGet("{id:length(24)}")]
+        public async Task<ActionResult<PmsCenter>> GetCenterByIdAsync(string id)
+        {
+            var center = await centerRepository.GetCenterByIdAsync(id);
+            if (center == null)
+            {
+                return NotFound($"Could not find center with id = {id}");
+            }
 
+            return Ok(center);
+        }
+
+        /// <summary>
+        /// Get a center by its Name
+        /// </summary>
+        /// <param name="name">The Name of the classe to get</param>
+        /// <returns></returns>
+        /// <response code="200">The center with the specified Name is returned</response>
+        /// <response code="404">The center with the specified Name does not exist in the Database</response>
+        [HttpGet("ByName/{name}")]
+        public async Task<ActionResult<PmsCenter>> GetCenterByNameAsync(string name)
+        {
+            var center = await centerRepository.GetCenterByNameAsync(name);
+            if (center == null)
+            {
+                return NotFound($"Could not find Pms Center with the name '{name}'");
+            }
+
+            return Ok(center);
+        }
     }
 }
