b4a9c49831415189a4aa12b386e6bfd6deaef461
Amaury Dewaersegger <amaury@mi8.be>
2022-02-23 10:00:05 +0100

    Add edit school form

--- DailyPmsShared/School.cs
+++ DailyPmsShared/School.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace DailyPmsShared
 {
@@ -6,20 +7,31 @@ namespace DailyPmsShared
     {
         public string SchoolID { get; set; }
 
+        [Required]
+        [StringLength(100, ErrorMessage = "Le nom doit comporter au moins 2 caractères !", MinimumLength = 2)]
         public string Name { get; set; }
 
+        [Required]
+        [StringLength(8, ErrorMessage = "Le nom ne peut pas comporter plus de 10 caractères !")]
         public string Moniker { get; set; }
 
+        [Required(ErrorMessage = "Veuillez compléter ce champ")]
         public string Street { get; set; }
 
+        [Required(ErrorMessage = "Veuillez compléter ce champ")]
         public int PostalCode { get; set; }
 
+        [Required(ErrorMessage = "Veuillez compléter ce champ")]
         public string City { get; set; }
 
+        [Required(ErrorMessage = "Veuillez compléter ce champ")]
         public string Phone { get; set; }
 
+        [Required]
+        [EmailAddress(ErrorMessage = "Veuillez insérer une adresse email valide")]
         public string Email { get; set; }
 
+        [Required(ErrorMessage = "Veuillez compléter ce champ")]
         public string DirectorName { get; set; }
 
         public string PmsCenterID { get; set; }
