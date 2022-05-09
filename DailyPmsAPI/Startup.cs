bb6bf7ac6fac89529bb9b6b7c320bb72dc9290bc
Amaury Dewaersegger <amaury@mi8.be>
2022-05-09 10:59:35 +0200

    Clean up comment

--- DailyPmsAPI/Startup.cs
+++ DailyPmsAPI/Startup.cs
@@ -25,29 +25,24 @@ namespace DailyPmsAPI
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-
-            //RegisterMongoDbClassMaps();
         }
 
         public IConfiguration Configuration { get; }
 
         public void ConfigureServices(IServiceCollection services)
         {
-            //  MongoDB
             services.AddSingleton<IMongoClient, MongoClient>(s =>
             {
                 var connectionString = Configuration.GetConnectionString("MongoConnection");
                 return new MongoClient(connectionString);
             });
 
-            //  SQL
             services.AddDbContext<SqlDbContext>(options =>
             {
                 var connectionStringSql = Configuration.GetConnectionString("SqlConnection");
                 options.UseSqlServer(connectionStringSql);
             });
 
-            //   BlobStorage
             services.AddSingleton(x =>
             {
                 var blobStorageConnectionString = Configuration.GetConnectionString("BlobStorageConnectionString");
@@ -55,27 +50,20 @@ namespace DailyPmsAPI
             });
             services.AddSingleton<IBlobService, BlobService>();
 
-            //*TEMP - TO REMOVE :
+            //  TO DO => Old repo - non generic, to replace with generic one
             services.AddTransient<IDbContext, MongoDbContext>();
-            //services.AddTransient<ISchoolRepository, MongoSchoolRepository>();
-            //services.AddTransient<IStudentRepository, MongoStudentRepository>();
             services.AddTransient<IClasseRepository, MongoClasseRepository>();
             services.AddTransient<IPmsCenterRepository, MongoPmsCenterRepository>();
             services.AddTransient<IAgentRepository, MongoAgentRepository>();
-            //*/
+            //
 
-            //Db + Repositories
             services.AddTransient<IDatabase, MongoDatabase>();
             services.AddTransient<IRepository<School>, SchoolRepository>();
             services.AddTransient<IRepository<Student>, StudentRepository>();
-            //services.AddTransient<IRepository<Agent>, AgentRepository>();
-            //services.AddTransient<IRepository<Class>, ClassRepository>();
-            //services.AddTransient<IRepository<PmsCenter>, PmsCenterRepository>();
-            //services.AddTransient<IRepository<PmsFile>, PmsFileRepository>();
+            
 
             services.AddControllers();
 
-            //  DEBUG api + client both on localhost (to avoid "Cross-origin" browser error)
             services.AddCors(options =>
             {
                 options.AddDefaultPolicy(
@@ -88,8 +76,7 @@ namespace DailyPmsAPI
                     });
             });
 
-            //   Swagger documentation
-            services.AddSwaggerGen(c =>    // Register the swagger generator, defining one or more swagger documents
+            services.AddSwaggerGen(c => 
             {
                 c.SwaggerDoc("v1", new OpenApiInfo
                 {
@@ -107,16 +94,13 @@ namespace DailyPmsAPI
                     }
                 });
 
-                //Use XML document to add infos in swagger doc
                 var xmlFilePath = Path.Combine(AppContext.BaseDirectory, "DailyPmsAPI.xml");
                 c.IncludeXmlComments(xmlFilePath);
             });
 
-            //  Authentication
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddMicrosoftIdentityWebApi(Configuration.GetSection("AzureAdB2C"));
 
-            //  Authorization
             services.AddAuthorization(options =>
             {
                 options.AddPolicy("Admin", policy => policy.RequireClaim("extension_Roles", "Admin"));
@@ -132,9 +116,9 @@ namespace DailyPmsAPI
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseSwagger();   // Enable middleware to serve generated swagger as JSON endpoint
+            app.UseSwagger();
 
-            app.UseSwaggerUI(c =>   // Enable middleware to serve swagger-ui (HTML, JS, CSS, ...). Specifying the swagger JSON endpoint
+            app.UseSwaggerUI(c =>
             {
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "Daily PMS API Documentation v1");
                 c.RoutePrefix = string.Empty;
@@ -157,65 +141,5 @@ namespace DailyPmsAPI
                 endpoints.MapFallbackToFile("index.html");
             });
         }
-
-        //void RegisterMongoDbClassMaps()
-        //{
-        //    BsonClassMap.RegisterClassMap<School>(cm =>
-        //    {
-        //        cm.AutoMap();
-        //        cm.MapIdMember(s => s._id).SetIdGenerator(StringObjectIdGenerator.Instance)
-        //                                  .SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(s => s.PmsCenterID).SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(s => s.ClasseIDs).SetSerializer(new EnumerableInterfaceImplementerSerializer<List<string>, string>(new StringSerializer(BsonType.ObjectId)));
-        //        cm.MapMember(s => s.StudentIDs).SetSerializer(new EnumerableInterfaceImplementerSerializer<List<string>, string>(new StringSerializer(BsonType.ObjectId)));
-        //    });
-        //    BsonClassMap.RegisterClassMap<Classe>(cm =>
-        //    {
-        //        cm.AutoMap();
-        //        cm.MapIdMember(c => c._id).SetIdGenerator(StringObjectIdGenerator.Instance)
-        //                                       .SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(c => c.SchoolID).SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(c => c.PmsIDs).SetSerializer(new EnumerableInterfaceImplementerSerializer<List<string>, string>(new StringSerializer(BsonType.ObjectId)));
-        //        cm.MapMember(c => c.StudentIDs).SetSerializer(new EnumerableInterfaceImplementerSerializer<List<string>, string>(new StringSerializer(BsonType.ObjectId)));
-        //    });
-        //    BsonClassMap.RegisterClassMap<Student>(cm =>
-        //    {
-        //        cm.AutoMap();
-        //        cm.MapIdMember(s => s._id).SetIdGenerator(StringObjectIdGenerator.Instance)
-        //                                  .SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        //cm.MapMember(s => s.BirthDate).SetSerializer(new DateTimeSerializer(dateOnly: true));
-        //        //cm.MapMember(s => s.RegistrationDate).SetSerializer(new DateTimeSerializer(dateOnly: true));
-        //        cm.MapMember(s => s.SchoolID).SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(s => s.ClasseID).SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(s => s.PmsFileID).SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //    });
-        //    BsonClassMap.RegisterClassMap<PmsFile>(cm =>
-        //    {
-        //        cm.AutoMap();
-        //        cm.MapIdMember(p => p._id).SetIdGenerator(StringObjectIdGenerator.Instance)
-        //                                        .SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(p => p.StudentID).SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(p => p.ActivityIDs).SetSerializer(new EnumerableInterfaceImplementerSerializer<List<string>, string>(new StringSerializer(BsonType.ObjectId)));
-        //    });
-        //    BsonClassMap.RegisterClassMap<PmsCenter>(cm =>
-        //    {
-        //        cm.AutoMap();
-        //        cm.MapIdMember(c => c._id).SetIdGenerator(StringObjectIdGenerator.Instance)
-        //                                       .SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(c => c.DirectorID).SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(c => c.PmsIDs).SetSerializer(new EnumerableInterfaceImplementerSerializer<List<string>, string>(new StringSerializer(BsonType.ObjectId)));
-        //        cm.MapMember(c => c.SchoolIDs).SetSerializer(new EnumerableInterfaceImplementerSerializer<List<string>, string>(new StringSerializer(BsonType.ObjectId)));
-        //    });
-        //    BsonClassMap.RegisterClassMap<Agent>(cm =>
-        //    {
-        //        cm.AutoMap();
-        //        cm.MapIdMember(a => a._id).SetIdGenerator(StringObjectIdGenerator.Instance)
-        //                                      .SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(a => a.CenterID).SetSerializer(new StringSerializer(BsonType.ObjectId));
-        //        cm.MapMember(a => a.SchoolIDs).SetSerializer(new EnumerableInterfaceImplementerSerializer<List<string>, string>(new StringSerializer(BsonType.ObjectId)));
-        //        cm.MapMember(a => a.PlanningIDs).SetSerializer(new EnumerableInterfaceImplementerSerializer<List<string>, string>(new StringSerializer(BsonType.ObjectId)));
-        //        cm.MapMember(a => a.DailyIDs).SetSerializer(new EnumerableInterfaceImplementerSerializer<List<string>, string>(new StringSerializer(BsonType.ObjectId)));
-        //    });
-        //}
     }
 }
