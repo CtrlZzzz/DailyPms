3b29427fd1dcd8435c0f1b32bd1f0499a0e64e78
Amaury Dewaersegger <amaury@mi8.be>
2022-05-24 16:54:50 +0200

    Add IntegrationTestFixture.cs + user secrets

---
+++ DailyPmsAPI/StartupTest.cs
@@ -0,0 +1,117 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Storage.Blobs;
+using DailyPmsAPI.Data;
+using DailyPmsAPI.Repositories;
+using DailyPmsAPI.Sql;
+using DailyPmsShared;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
+using MongoDB.Driver;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace DailyPmsAPI
+{
+    public class StartupTest
+    {
+        public StartupTest(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton<IMongoClient, MongoClient>(s =>
+            {
+                var connectionString = Configuration.GetConnectionString("MongoConnection");
+                return new MongoClient(connectionString);
+            });
+
+            //services.AddDbContext<SqlDbContext>(options =>
+            //{
+            //    var connectionStringSql = Configuration.GetConnectionString("SqlConnection");
+            //    options.UseSqlServer(connectionStringSql);
+            //});
+
+            //services.AddSingleton(x =>
+            //{
+            //    var blobStorageConnectionString = Configuration.GetConnectionString("BlobStorageConnectionString");
+            //    return new BlobServiceClient(blobStorageConnectionString);
+            //});
+
+            //   TO DO => Old repo - non generic, to replace with generic one
+            services.AddTransient<IDbContext, MongoDbContext>();
+            services.AddTransient<IClasseRepository, MongoClasseRepository>();
+            services.AddTransient<IPmsCenterRepository, MongoPmsCenterRepository>();
+            services.AddTransient<IAgentRepository, MongoAgentRepository>();
+            //
+
+            services.AddTransient<IDatabase, MongoDatabase>();
+            services.AddTransient<IRepository<School>, SchoolRepository>();
+            services.AddTransient<IRepository<Student>, StudentRepository>();
+
+
+            //services.AddControllers();
+
+            //services.AddCors(options =>
+            //{
+            //    options.AddDefaultPolicy(
+            //        builder =>
+            //        {
+            //            builder.WithOrigins("https://localhost:44367")
+            //                    .AllowAnyHeader()
+            //                    .AllowAnyMethod()
+            //                    .AllowCredentials();
+            //        });
+            //});
+
+            //services.AddRazorPages();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //}
+
+            //app.UseHttpsRedirection();
+            //app.UseStaticFiles();
+
+            //app.UseRouting();
+
+            //app.UseCors();
+
+            //app.UseAuthentication();
+            //app.UseAuthorization();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //    endpoints.MapRazorPages();
+            //    endpoints.MapFallbackToFile("index.html");
+            //});
+        }
+
+        //string GetVaultSecret(string secretName)
+        //{
+        //    var client = new SecretClient(new Uri("https://dailypmsvault.vault.azure.net/"), new ManagedIdentityCredential());
+        //    var secret = client.GetSecret(secretName).Value;
+
+        //    return secret.Value;
+        //}
+    }
+}
+
--- TEST.DailyPmsAPI.IntegrationTests/IntegrationTestFixture.cs
+++ TEST.DailyPmsAPI.IntegrationTests/IntegrationTestFixture.cs
@@ -1,10 +1,55 @@
 ﻿using System;
+using System.Net.Http;
+using DailyPmsAPI;
+using DailyPmsShared;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using Xunit;
+
 namespace TEST.DailyPmsAPI.IntegrationTests
 {
-    public class IntegrationTestFixture
+    public class IntegrationTestFixture<T> : IDisposable, IClassFixture<WebApplicationFactory<StartupTest>> where T : class, IEntity
     {
-        public IntegrationTestFixture()
+        const string DBNAME = "PmsDbTest";
+
+        protected readonly HttpClient testingClient;
+
+        protected IConfiguration configuration;
+
+        protected IntegrationTestFixture(WebApplicationFactory<StartupTest> factory, IConfiguration configuration)
         {
+            testingClient = factory.CreateClient();
+            this.configuration = configuration;
+
+            ConfigureMongo();
+        }
+
+        public string? CollectionName { get; private set; }
+        public IMongoDatabase? MongoDb { get; private set; }
+        public IMongoCollection<T>? MongoCollection { get; private set; }
+
+
+        public void Dispose()
+        {
+            testingClient.Dispose();
+            MongoDb?.DropCollection(CollectionName);
+        }
+
+        string GetUserSecret(string secretName)
+        {
+            return configuration.GetConnectionString(secretName);
+        }
+
+        void ConfigureMongo()
+        {
+            var MongoConnectionString = GetUserSecret("MongoConnection");
+            var settings = MongoClientSettings.FromConnectionString(MongoConnectionString);
+            settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+            var mongoClient = new MongoClient(settings);
+            MongoDb = mongoClient.GetDatabase(DBNAME) ?? throw new NullReferenceException(nameof(MongoDb));
+            CollectionName = typeof(T).Name + "s";
+            MongoCollection = MongoDb.GetCollection<T>(CollectionName) ?? throw new NullReferenceException(nameof(MongoCollection));
         }
     }
 }
